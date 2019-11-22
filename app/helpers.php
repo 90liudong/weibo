3b3b10da510f7bf05312476e35c9aab73282f2e9
刘东 <1436899717@qq.com>
2019-11-22 06:16:36 +0000

    调试pgsql

--- app/helpers.php
+++ app/helpers.php
@@ -1,23 +1,23 @@
 <?php
 function get_db_config()
 {
-    echo getenv('IS_IN_HEROKU');
-    print_r(parse_url(getenv("DATABASE_URL")));
-    exit();
     if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
         return $db_config = [
             'connection' => 'pgsql',
             'host'       => $url["host"],
+            'port'       => $url["port"],
             'database'   => substr($url["path"], 1),
             'username'   => $url["user"],
-            'password'   => $url["pass"]];
+            'password'   => $url["pass"]
+        ];
     } else {
         return $db_config = [
             'connection' => env('DB_CONNECTION', 'mysql'),
             'host'       => env('DB_HOST', 'localhost'),
             'database'   => env('DB_DATABASE', 'forge'),
             'username'   => env('DB_USERNAME', 'forge'),
-            'password'   => env('DB_PASSWORD', '')];
+            'password'   => env('DB_PASSWORD', '')
+        ];
     }
 }
